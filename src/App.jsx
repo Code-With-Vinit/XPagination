4fbc7b4b65f16c54dd44967a509b062fe26e5648
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-06-06 23:19:12 +0530

    Commited Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+
+
+const Endpoint="https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
 
 
 function App() {
+
+  useEffect(()=>
+    {
+      async function fetchDetails(){
+      try{
+        const response=await fetch(Endpoint);
+        const data=await response.json();
+        console.log(data);
+      }
+      catch(e)
+      {
+        alert("failed to fetch data");
+      }
+
+      }
+     fetchDetails();
+    },[])
  
   return (
     <>
       <h1>XPagination</h1>
+
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>ROLE</th>
+        </thead>
+      </table>
     </>
   )
 }
