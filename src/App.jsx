6378590b2533d849b4bc2dcb24b767099af43d3d
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-06-10 16:41:10 +0530

    implemented the Pagination feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from 'react'
+import "./App.css";
 
 
 
 const Endpoint="https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
 
 
+
+
+
+
 function App() {
 
   const [data,setData]=useState([]);
+  const [currentPage,setCurrentPage]= useState(1);
+  const recordsPerPage=10;
+  const lastIndex=currentPage*recordsPerPage;
+  const firstIndex=lastIndex-recordsPerPage;
+  const records=data.slice(firstIndex,lastIndex);
+  const nPages=Math.ceil(data.length/recordsPerPage);
+  const numbers=[...Array(nPages+1).keys()].slice(1);
+
 
   useEffect(()=>
     {
@@ -29,8 +42,8 @@ function App() {
     },[])
  
   return (
-    <>
-      <h1>XPagination</h1>
+    <div className="Container">
+      <h1>Employee Data Table</h1>
 
       <table>
         <thead>
@@ -43,7 +56,7 @@ function App() {
         </thead>
 
         <tbody>
-          {data.map((d,i)=>(
+          {records.map((d,i)=>(
             <tr key={i}>
                 <td>{d.id}</td>
                 <td>{d.name}</td>
@@ -53,8 +66,52 @@ function App() {
           ))}
         </tbody>
       </table>
-    </>
+
+      <nav>
+        <ul>
+          <button>
+            <a href="#"
+            onClick={prePage}
+            >
+              Previous
+            </a>
+          </button>
+          {
+            numbers.map((n,i)=>(
+              <button className={`${currentPage === n ? 'active' :'unactive'}`} key={i}>
+                  <a href="#" onClick={()=>ChangeCPage(n)}>{n}</a>
+              </button>
+            ))
+          }
+          <button>
+            <a href="#"
+            onClick={nextPage}
+            >
+              Next
+            </a>
+          </button>
+        </ul>
+      </nav>
+    </div>
   )
+
+function prePage(){
+  if(currentPage!==1)
+  {
+    setCurrentPage(currentPage-1);
+  }
+}
+
+function nextPage(){
+  if(currentPage!==nPages)
+  {
+    setCurrentPage(currentPage+1);
+  }
+}
+function ChangeCPage(id){
+  setCurrentPage(id);
+}
+
 }
 
 export default App
